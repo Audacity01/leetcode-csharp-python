c233b2b90776f9322dfe973d94156ea25a34aca7
Vaibhav <vaibhav.joshi224152@gmail.com>
2026-03-30 00:01:14 +0530

    add initial leetcode solutions — arrays category

---
+++ arrays/contains-duplicate/Solution.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var seen = new HashSet<int>();
+        foreach (var n in nums)
+        {
+            if (!seen.Add(n))
+                return true;
+        }
+        return false;
+    }
+}
